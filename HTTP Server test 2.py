8038e0a0f5f2e14942e6aac41b50fe7c350a3da1
Giankess <gianfrancokessler@gmail.com>
2016-11-11 18:05:06 +0100

    Webserverplanung und test überarbeitet
    Ich habe mich nochmals über die Funktionen vom http handler und das
    string.split informiert und begonnen meinen handler aufzubauen

--- HTTP Server test 2.py
+++ HTTP Server test 2.py
@@ -6,13 +6,23 @@ PORT = 8000
 #Port festlegen
 
 Handler = http.server.SimpleHTTPRequestHandler
-#Handler festlegen, welcher Anfragen bearbeitet
+#Handler festlegen, welcher Anfragen bearbeitet<-Ersetzen
+#Start Konstruktor
+class MyHandler:
+    #Allgemeine Variablen für den Handler kommen hier rein
+    def Handle(self):
+        #String spalten um Methode etc. zu bekommen
+        def Parcer():
+            string = request
+            string.split(' ')
+
+
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 #festlegen, worüber der Server läuft
 
 print("serving at port", PORT)
-#NAchricht dass der Server über Port 8000 läuft
+#Nachricht dass der Server über Port 8000 läuft
 
 httpd.serve_forever()
 #Lässt Server laufen, bis das Programm beendet wird
