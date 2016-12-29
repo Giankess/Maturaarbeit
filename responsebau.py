d27fd7696a10204993c7f44ffb9603b6d42a24d0
Giankess <gianfrancokessler@gmail.com>
2016-12-29 13:39:18 +0100

    Responsebau verbessert
    responsebau für Put und Error erweitert

--- responsebau.py
+++ responsebau.py
@@ -18,8 +18,25 @@ class responsebau:
     def Putresponse(self):
         f = open(path, w)
         #was schreiben? beim parser nicht sortiert für dictionary
-        f.write()
-
+        f.write(Body)
+        #respnse erstellen
+        print(Version 201 Created
+        Date: time.strftime( %a, %d %b %Y %H: %M: %S %Z)
+        Host: Requestinhalt["Host"]
+        Server: Python
+        Accept - Ranges: bytes
+        Content - Lenght: f.size
+        Content - Type: text/html
+        f.read())
     def Errorresponse(self:
-
+        print(Version 404 Not Found
+        Date: time.strftime( %a, %d %b %Y %H: %M: %S %Z)
+        Server: Python
+        Host: Requestinhalt[Host]
+        )
     def Errorresponse1(self):
+        print(Version 501 Not Implemented
+        Date: time.strftime( %a, %d %b %Y %H: %M: %S %Z)
+        Server: Python
+        Host: Requestinhalt[Host]
+        )
