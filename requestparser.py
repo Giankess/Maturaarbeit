1986c65b435233a19cf8e4ec2af06bf4879ee7a2
Giankess <gianfrancokessler@gmail.com>
2016-12-30 15:29:12 +0100

    requestparser+test
    verschiedenes um den parser zum laufen zu bringen. Ich habe jetzt alles
    in einem loop und einen test. jedoch geht das importieren von files
    nicht.

--- requestparser.py
+++ requestparser.py
@@ -1,23 +1,24 @@
 class requestparser:
     def parse(self):
-        string = self.request
+        #string = self.request
         # string in Zeilen spalten
-        zeilen = string.split(\n)
+        zeilen = string.split('\n')
         # erste Zeile spalten und Variablen entnehmen
-        zeile1 = zeilen[0]
-        parts1 = zeile1.split(' ')
-        Method = parts1[0]
-        Path = parts1[1]
-        Version = parts[2]
-        zeilen = zeilen - zeilen[0]
-        i = 0
-        while zeile != "":
-            for zeile in zeilen[]:
-                parts = zeile.split(:)
-                Requestinhalt = {}
-                Requestinhalt[parts[0]]=parts[1]
-                i=i+1
+        #zeile1 = zeilen[0]
+        #parts1 = zeile1.split(' ')
+        #Method = parts1[0]
+        #Path = parts1[1]
+        #Version = parts[2]
+        Requestinhalt = {}
         Body = ""
-        while zeile != "":
-            i=i+1
-            Body = Body+zeile[i]
+        for zeile in zeilen:
+            if ":" in zeile:
+                parts = zeile.split(':')
+                Requestinhalt[parts[0]]=parts[1]
+            elif "HTTP" in zeile:
+                parts = zeile.split(' ')
+                Method = parts[0]
+                Path = parts[1]
+                Version = parts[2]
+            else:
+                Body = Body+zeile
---
+++ test_requestparser.py
@@ -0,0 +1,8 @@
+import requestparser.py
+request = "GET /hello.txt HTTP/1.1"
+parse(string)
+print(Version)
+print(Method)
+print(Path)
+print(Requestinhalt)
+print(Body)
