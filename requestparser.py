602c651bade1078f90e71a207b006fcadbdd299f
Giankess <gianfrancokessler@gmail.com>
2017-01-22 17:44:39 +0100

    Index.html und Maturaarbeit
    Ich habe Index.html nun endlich auch zu einer HTML-Datei gemacht. Ich
    habe das Vorwort, die Einleitung, ein Kapitel zu Problemen beim Code und
    das Schlusswort geschrieben. Vermutlich werden jedoch noch Anpassungen
    anfallen.

--- requestparser.py
+++ requestparser.py
@@ -3,12 +3,6 @@ class requestparser:
         string = request
         # string in Zeilen spalten
         zeilen = string.split('\n')
-        # erste Zeile spalten und Variablen entnehmen
-        #zeile1 = zeilen[0]
-        #parts1 = zeile1.split(' ')
-        #Method = parts1[0]
-        #Path = parts1[1]
-        #Version = parts[2]
         self.Requestinhalt = {}
         self.Body = ""
         #alle Zeilen Parsen
