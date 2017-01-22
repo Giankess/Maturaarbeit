f32a7794049a60b7667572fec4081cd317c326e7
Giankess <gianfrancokessler@gmail.com>
2017-01-22 20:42:44 +0100

    Fehlerbehebung
    kleine Fehlerbehebung wegen Fehler bei Responsebau

--- ResponseBuilder.py
+++ ResponseBuilder.py
@@ -1,19 +1,20 @@
 import time
+import os
 class ResponseBuilder:
-    Vorlage = ("""{version} {sc}
+    vorlage = ("""{version} {sc}
         Date: {date}
         Server: Python
         Host: {host}
         {rest}""")
-    Vorlage2 = """Accept-Ranges: bytes
+    vorlage2 = """Accept-Ranges: bytes
             Content - Lenght: {size}
             Content-Type: {contenttype}
             """
     def Getresponse(self, ps):
         try:
             f = open(ps.Path)
-            response = self.Vorlage.format(version = ps.Version, sc = "200 OK", date =\
-                time.strftime("%a, %d %b %Y %H:%M:%S %Z"), host = ps.Requestinhalt['Host'],rest = self.Vorlage2.format(size = f.size, contenttype = ps.Requestinhalt['Content-Type']))
+            response = self.vorlage.format(version = ps.Version, sc = "200 OK", date =\
+                time.strftime("%a, %d %b %Y %H:%M:%S %Z"), host = ps.Requestinhalt['Host'],rest = self.vorlage2.format(size = os.path.getsize(ps.Path), contenttype = ps.Requestinhalt['Content-Type']))
             file = f.read()
             file = file.encode("'",ps.code,"'")
             response = response.encode("UTF-8")
@@ -27,8 +28,8 @@ class ResponseBuilder:
         f = open(ps.Path, w)
         f.write(Body)
         #response erstellen
-        response = self.Vorlage.format(version = ps.Version, sc = "201 Created", date =\
-            time.strftime("%a, %d %b %Y %H:%M:%S %Z"), host = ps.Requestinhalt['Host'],rest = self.Vorlage2.format(size = ps.f.size, contenttype = ps.Requestinhalt['Content-Type']))
+        response = self.vorlage.format(version = ps.Version, sc = "201 Created", date =\
+            time.strftime("%a, %d %b %Y %H:%M:%S %Z"), host = ps.Requestinhalt['Host'],rest = self.vorlage2.format(size = os.path.getsize(ps.Path), contenttype = ps.Requestinhalt['Content-Type']))
         file = ps.f.read()
         file = file.encode("'",ps.code,"'")
         response = response.encode("UTF-8")
@@ -36,7 +37,7 @@ class ResponseBuilder:
         return response
     def Errorresponse(self, ps):
         #Error bei nicht gefundener Datei
-        response = self.Vorlage.format(version = ps.Version, sc = "404 Not Found", date =\
+        response = self.vorlage.format(version = ps.Version, sc = "404 Not Found", date =\
             time.strftime("%a, %d %b %Y %H:%M:%S %Z"), host = ps.Requestinhalt['Host'],rest = """<html><head><title>Not Found</title></head><body>
 Sorry, the object you requested was not found.
 </body><html>""")
@@ -44,7 +45,7 @@ Sorry, the object you requested was not found.
         return response
     def Errorresponse1(self, ps):
         #Error bei unbekannter Methode
-        response = self.Vorlage.format(version = ps.Version, sc = "501 Not Implemented", date =\
+        response = self.vorlage.format(version = ps.Version, sc = "501 Not Implemented", date =\
             time.strftime("%a, %d %b %Y %H:%M:%S %Z"), host = ps.Requestinhalt['Host'],rest = """<html><head><title>Not Found</title></head><body>
 Sorry, the object you requested was not found.
 </body><html>""")
